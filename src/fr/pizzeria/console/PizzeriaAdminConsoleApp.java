84b81a80355c6a7a9397b3cd00db1c933d053482
DROUAUD Aurélien <a.drouaud@gmail.com>
2019-03-18 18:36:03 +0100

    TP 5

--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,43 +1,133 @@
 package fr.pizzeria.console;
+
 import java.util.*;
+import fr.pizzeria.model.Pizza;
 
 public class PizzeriaAdminConsoleApp {
 	public static void main(String[] args) {
-		System.out.println("***PIZZA ADMINISTRATION****\n 1. Lister les pizzas\n 2. Ajouter une nouvelle pizza\n 3. Mettre à jour une pizza\n 4. Supprimer une Pizza\n 5. Sortir" );
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
+		Pizza[] pizza = new Pizza[100];
+		pizza[0] = new Pizza("PEP", "Pépéroni", 12.50);
+		pizza[1] = new Pizza("MAR", "Margherita", 14.00);
+		pizza[2] = new Pizza("REIN", "La Reine", 11.50);
+		pizza[3] = new Pizza("FRO", "La 4 fromages", 12.00);
+		pizza[4] = new Pizza("CAN", "La cannibale", 12.50);
+		pizza[5] = new Pizza("SAV", "La savoyarde", 13.00);
+		pizza[6] = new Pizza("ORI", "L'orientale", 13.50);
+		pizza[7] = new Pizza("IND", "L'indienne", 14.00);
 
-		while(n>5){
-		switch (n) {
-		
-			case 1 : 
+		Scanner sc = new Scanner(System.in);
+		int n = 0;
+
+		while (n < 5) {
+			System.out.println(
+					"***PIZZA ADMINISTRATION****\n 1. Lister les pizzas\n 2. Ajouter une nouvelle pizza\n 3. Mettre à jour une pizza\n 4. Supprimer une Pizza\n 5. Sortir");
+			n = sc.nextInt();
+			switch (n) {
+
+			case 1:
 				System.out.println("1. Lister les pizzas");
-				
+				affiche(pizza);
 				break;
-			
-			case 2 : 
+			case 2:
 				System.out.println("2. Ajouter une nouvelle pizza");
+				ajouter(pizza);
 				break;
-			
-			case 3 :
-				System.out.println("3. Mettre à jour une pizza");			
-				break;
-			
-			case 4 :
-				System.out.println("4. Supprimer une Pizza");
-				break;
-		
-			case 5 : 
-				System.out.println("99. Sortir");
-				break;
-			default :
-				System.out.println("Merci de choisir dans le menu");
-				System.out.println("***PIZZA ADMINISTRATION****\n 1. Lister les pizzas\n 2. Ajouter une nouvelle pizza\n 3. Mettre à jour une pizza\n 4. Supprimer une Pizza\n 5. Sortir ☹" );
-				
-		}
-	      n=sc.nextInt();
-		}	
-		sc.close();
-}
 
+			case 3:
+				System.out.println("3. Mettre à jour une pizza");
+				recup(pizza);
+				break;
+
+			case 4:
+				System.out.println("4. Supprimer une Pizza");
+				supp(pizza);
+				break;
+
+			case 5:
+				System.out.println("5. Sortir");
+				break;
+			default:
+				System.out.println("Merci de choisir dans le menu");
+			}
+
+		}
+
+		sc.close();
 	}
+
+	public static void affiche(Pizza[] pizza) {
+		for (Pizza pi : pizza)
+			if (pi != null) {
+				{
+					System.out.println(pi.getCode() + " -> " + pi.getLibelle() + " (" + pi.getPrix() + ") ");
+				}
+			}
+	}
+
+	public static void ajouter(Pizza[] pizza) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir le code :");
+		String coderecup = sc.nextLine();
+		
+		
+		
+		System.out.println("Veuillez saisir le nom (sans espace)");
+		String nomrecup = sc.nextLine();
+
+		System.out.println("Veuillez saisir le prix :");
+		String prixrecup = sc.nextLine();
+		Double prixrecup2 = Double.parseDouble(prixrecup);
+		
+		for (int i=0; i<pizza.length; i++) {
+			if (pizza[i]==null) {
+				pizza[i] = new Pizza(coderecup, nomrecup, prixrecup2);
+				break;
+
+			}
+		}
+		
+		
+	}
+	
+	public static void recup(Pizza[] pizza) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quel code souhaitez vous modifier ?");
+		String coderecup = sc.nextLine();
+			for(int j=0; j<pizza.length; j++) {
+				if(pizza[j].getCode().equals(coderecup)) {
+					System.out.println("Veuillez mettre à jour le code :");
+					String coderecup2 = sc.nextLine();
+
+					System.out.println("Veuillez mettre à jour le nom (sans espace)");
+					String nomrecup = sc.nextLine();
+
+					System.out.println("Veuillez mettre à jour le prix :");
+					String prixrecup = sc.nextLine();
+					Double prixrecup2 = Double.parseDouble(prixrecup);
+					pizza[j].setCode(coderecup2);
+					pizza[j].setLibelle(nomrecup);
+					pizza[j].setPrix(prixrecup2);
+					break;
+				}}
+			}
+			public static void supp(Pizza[] pizza) {
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Quel code souhaitez vous supprimer ?");
+				String coderecup = sc.nextLine();
+					for(int j=0; j<pizza.length; j++) {
+						if(pizza[j].getCode().equals(coderecup)) {
+							pizza[j] = null;
+							break;
+						}
+					}		
+				
+		
+		
+
+			}
+		
+		
+		
+	}
+
+
--- src/fr/pizzeria/model/Pizza.java
+++ src/fr/pizzeria/model/Pizza.java
@@ -1,15 +1,17 @@
 package fr.pizzeria.model;
 
 public class Pizza {
+	Pizza pizza;
 	int id;
 	String code;
 	String libelle;
 	double prix;
-	int cpt;
+	static int cpt;
+
 
 public Pizza (String code, String libelle, double prix) {
-	cpt++;
-	this.id=cpt;
+	Pizza.cpt++;
+	this.id=Pizza.cpt;
 	this.code=code;
 	this.libelle=libelle;
 	this.prix=prix;	
@@ -23,8 +25,28 @@ public Pizza(int id, String code, String libelle, double prix) {
 	}
 
 
+
+	
+
+
 public int getId() {
 	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public void setCode(String code) {
+	this.code = code;
+}
+
+public void setLibelle(String libelle) {
+	this.libelle = libelle;
+}
+
+public void setPrix(double prix) {
+	this.prix = prix;
 }
 
 public String getCode() {
